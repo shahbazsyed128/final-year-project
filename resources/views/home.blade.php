1c0eb635562a829e909ef4245e52f6335a370b90
Shahbaz kazmi <shahbazsyed128@gmail.com>
2021-08-08 23:01:51 +0500

    Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,7 +60,7 @@
                             <div class="icon">
                                 <i class="ion ion-stats-bars"></i>
                             </div>
-                            <a href="{{ route('user') }}" class="small-box-footer">More info <i
+                            <a href="{{ route('pending') }}" class="small-box-footer">More info <i
                                     class="fas fa-arrow-circle-right"></i></a>
 
                         </div>
